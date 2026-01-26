4791b3d6a434d61ecc4538f4491bb4f41f23a0e1
mariawozniak <maria@m-english.org>
2026-01-26 11:47:18 +0100

    Update course.js

--- public/course.js
+++ public/course.js
@@ -179,10 +179,13 @@ app.innerHTML = renderContent();
 `}
 
 
-  <div id="content">
+ <div id="content">
+  <div class="module-inner">
     ${renderContent()}
     ${renderFinalFeedback()}
   </div>
+</div>
+
 `;
 }
 
